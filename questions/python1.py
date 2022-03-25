4a02576746c904aa374366cd80294e7013ae3768
Harry <harry.leventon@gmail.com>
2022-03-25 13:53:16 +0000

    q 2

--- questions/python1.py
+++ questions/python1.py
@@ -69,7 +69,15 @@ def max_length(string1, string2):
 	# No hints for this question
 
 def two(arg1):
+	if (arg1 % 3 == 0) and (arg1 % 5 != 0):
+		return "fizz"
+	elif (arg1 % 3 == 0) and (arg1 % 5 == 0):
+		return "fizzbuzz"
+	elif arg1 % 5 == 0:
+		return "buzz"
 	return "null"
+
+# print(two(8))
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
